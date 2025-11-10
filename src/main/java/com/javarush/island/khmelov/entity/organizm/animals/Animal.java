e4fdc026610ccd5ae1c3125d2d7b670b12b02837
akhmelev <akhmelev@gmail.com>
2025-11-10 18:50:23 +0300

    Cons. 2.3 Fat Jar + Monitor

--- src/main/java/com/javarush/island/khmelov/entity/organizm/animals/Animal.java
+++ src/main/java/com/javarush/island/khmelov/entity/organizm/animals/Animal.java
@@ -9,6 +9,8 @@ import com.javarush.island.khmelov.entity.organizm.Limit;
 import com.javarush.island.khmelov.entity.organizm.Organism;
 import com.javarush.island.khmelov.entity.organizm.Organisms;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public abstract class Animal extends Organism implements Eating, Reproducible, Movable {
 
     public Animal(String name, String icon, Limit limit) {
@@ -29,9 +31,10 @@ public abstract class Animal extends Organism implements Eating, Reproducible, M
 
     @Override
     public boolean move(Cell startCell) {
-        int countStep = this
+        int maxCountStep = this
                 .getLimit()
                 .getMaxSpeed();
+        int countStep = 1+ThreadLocalRandom.current().nextInt(maxCountStep+1);
         Cell destinationCell = startCell.getNextCell(countStep);
         return safeMove(startCell, destinationCell);
     }
@@ -42,14 +45,13 @@ public abstract class Animal extends Organism implements Eating, Reproducible, M
     }
 
     private boolean safeSpawnAnimal(Cell cell) {
-        cell.getLock().lock();
-        try {
+        synchronized (cell.monitor()) {
             Organisms organisms = cell.getResidents().get(getType());
             double maxWeight = getLimit().getMaxWeight();
             if (isHere(cell) && getWeight() > maxWeight / 2 &&
-                    organisms.contains(this) &&
-                    (organisms.getLimit().getFlockSize() > 1 || organisms.size() >= 2) &&
-                    organisms.size() < getLimit().getMaxCountInCell()
+                organisms.contains(this) &&
+                (organisms.getLimit().getFlockSize() > 1 || organisms.size() >= 2) &&
+                organisms.size() < getLimit().getMaxCountInCell()
             ) {
                 //all animals lose weight after the birth of offspring
                 double childWeight = getWeight() / 2;
@@ -59,8 +61,6 @@ public abstract class Animal extends Organism implements Eating, Reproducible, M
                 organisms.add(clone);
                 return true;
             }
-        } finally {
-            cell.getLock().unlock();
         }
         return false;
     }
